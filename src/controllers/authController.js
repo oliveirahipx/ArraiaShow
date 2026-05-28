ad84d8878a45c4af706e92806e63b6a963ab8a75
miguel oliveira <migueloliveira.hipx@gmail.com>
2026-05-28 16:37:58 -0300

    'Update'

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -57,27 +57,4 @@ export const verificarToken = (req, res, next) => {
     });
 };
 
-// Middleware para verificar se o usuário é barraqueiro
-export const verificarBarraca = (req, res, next) => {
-    if (req.user.tipo !== 'barraqueiro') {
-        return res.status(403).json({ message: "Acesso negado: apenas barraqueiros" });
-    }
-    next();
-};
 
-
-// Middleware para verificar se o usuário é organizador
-export const verificarOrganizador = (req, res, next) => {
-    if (req.user.tipo !== 'organizador') {
-        return res.status(403).json({ message: "Acesso negado: apenas organizadores" });
-    }
-    next();
-};
-
-// Middleware para verificar se o usuário é participante
-export const verificarParticipante = (req, res, next) => {
-    if (req.user.tipo !== 'participante') {
-        return res.status(403).json({ message: "Acesso negado: apenas participantes" });
-    }
-    next();
-};
